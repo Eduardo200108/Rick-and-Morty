f0873752f68604b10f08447e41bb3f8f95862459
Eduardo200108 <eduardo.19070053@itsmotul.edu.mx>
2023-02-27 17:38:56 -0600

    Rick and Morty

--- Rick and morty/src/App.vue
+++ Rick and morty/src/App.vue
@@ -8,9 +8,9 @@ import HelloWorld from "./components/HelloWorld.vue";
     <img
       alt="Vue logo"
       class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
+      src="@/assets/vector.svg"
+      width="600"
+      height="500"
     />
 
     <div class="wrapper">
