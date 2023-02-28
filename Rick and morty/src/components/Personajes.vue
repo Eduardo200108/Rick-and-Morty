990f46f612ea355e6cf89bc6873b29e5b3fe1d17
Eduardo200108 <eduardo.19070053@itsmotul.edu.mx>
2023-02-28 10:31:47 -0600

    rick and morty

--- Rick and morty/src/components/Personajes.vue
+++ Rick and morty/src/components/Personajes.vue
@@ -1,60 +1,55 @@
 <script>
-import axios from 'axios'
+import axios from "axios";
 
-let API_URL = `https://rickandmortyapi.com/api/character`
+let API_URL = `https://rickandmortyapi.com/api/character`;
 
 export default {
-
   data() {
     return {
       info: [],
       personajes: [],
-      cont:2
-    }
+      cont: 2,
+    };
   },
 
   mounted() {
-    axios.get(API_URL)
-      .then((response) => {
-        this.info = response.data.info;
-        this.personajes = response.data.results;
-      })
+    axios.get(API_URL).then((response) => {
+      this.info = response.data.info;
+      this.personajes = response.data.results;
+    });
   },
 
   methods: {
     pag(num) {
-      API_URL='https://rickandmortyapi.com/api/character/?page='+num
-      console.log(API_URL)
-      axios.get(API_URL)
-      .then((response) => {
-        console.log(response.config)
+      API_URL = "https://rickandmortyapi.com/api/character/?page=" + num;
+      console.log(API_URL);
+      axios.get(API_URL).then((response) => {
+        console.log(response.config);
         this.info = response.data.info;
         this.personajes = response.data.results;
-      })
-      this.cont++
-    }
+      });
+      this.cont++;
+    },
   },
-
-}
-
+};
 </script>
 
 <template>
-  <h2 class="flex text-center justify-center gap-10 p-5">Hay {{ info.count }} personajes en el programa de Rick & Morty </h2>
+  <h2 class="flex text-center justify-center gap-10 p-5">
+    Hay {{ info.count }} personajes en el programa de Rick & Morty
+  </h2>
   <button @click="pag(cont)">página {{ cont }}</button>
   <ul>
-    <li v-for="p in personajes" class="flex text-center justify-center gap-10 p-5"><!--centrar imagenes-->
-      <a >{{ p.name }} id:{{ p.id }} estado:{{ p.status }}</a> 
-      <img v-bind:src="p.image"  alt="no image"><!--lector de imagenes-->
-
+    <li v-for="p in personajes" class="flex text-center justify-center gap-10 p-5">
+      <!--centrar imagenes-->
+      <a>{{ p.name }} id:{{ p.id }} estado:{{ p.status }}</a>
+      <img v-bind:src="p.image" alt="no image" /><!--lector de imagenes-->
     </li>
   </ul>
 </template>
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=Patrick+Hand+SC&display=swap');
-*{
-  font-family: 'Patrick Hand SC', cursive;
+@import url("https://fonts.googleapis.com/css2?family=Patrick+Hand+SC&display=swap");
+* {
+  font-family: "Patrick Hand SC", cursive;
 }
-
 </style>
-<!style es donde esta el fondo del texto-->
